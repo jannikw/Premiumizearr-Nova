JackDallas/Premiumizearr

Refactor in to app structure, make all config options reloadable

web/src/pages/Config.svelte
@@ -13,6 +13,9 @@
     CheckmarkFilled,
     AddFilled,
     TrashCan,
+    HelpFilled,
+    MisuseOutline,
+    WatsonHealthRotate_360,
   } from "carbon-icons-svelte";
   import { CalculateAPIPath } from "../Utilities/web_root";
 
@@ -26,10 +29,17 @@
     SimultaneousDownloads: 0,
     Arrs: [],
   };
+  const ERR_SAVE = "Error Saving Config";
+  const ERR_TEST = "Error Testing *arr client";
+
+  let arrTesting = [];
+  let arrTestIcons = [];
+  let arrTestKind = [];
 
   let inputDisabled = true;
 
   let errorModal = false;
+  let errorTitle = ERR_SAVE;
   let errorMessage = "";
 
   let saveIcon = Save;
@@ -39,6 +49,12 @@
     fetch(CalculateAPIPath("api/config"))
       .then((response) => response.json())
       .then((data) => {
+        if (Array.isArray(data.Arrs)) {
+          for (let i = 0; i < data.Arrs.length; i++) {
+            SetTestArr(i, HelpFilled, "secondary", false);
+          }
+        }
+
         config = data;
         inputDisabled = false;
       })
@@ -66,14 +82,16 @@
           }, 1000);
         } else {
           errorMessage = data.status;
+          errorTitle = ERR_SAVE;
           errorModal = true;
           getConfig();
         }
       })
       .catch((error) => {
         console.error("Error: ", error);
-        errorModal = true;
+        errorTitle = ERR_SAVE;
         errorMessage = error;
+        errorModal = true;
         setTimeout(() => {
           getConfig();
         }, 1500);
@@ -97,6 +115,50 @@
     config.Arrs = [...config.Arrs];
   }
 
+  function TestArr(index) {
+    SetTestArr(index, WatsonHealthRotate_360, "secondary", true);
+
+    fetch(CalculateAPIPath("api/testArr"), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(config.Arrs[index]),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.succeeded) {
+          SetTestArr(index, CheckmarkFilled, "secondary", false);
+        } else {
+          SetTestArr(index, MisuseOutline, "danger", false);
+          errorTitle = ERR_TEST;
+          errorMessage = data.status;
+          errorModal = true;
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+        SetTestArr(index, MisuseOutline, "danger", false);
+        errorTitle = ERR_TEST;
+        errorMessage = error;
+        errorModal = true;
+      });
+  }
+
+  function UntestArr(index) {
+    SetTestArr(index, HelpFilled, "secondary", false);
+  }
+
+  function SetTestArr(index, icon, kind, testing) {
+    arrTesting[index] = testing;
+    arrTestIcons[index] = icon;
+    arrTestKind[index] = kind;
+    
+    arrTesting = [...arrTesting];
+    arrTestIcons = [...arrTestIcons];
+    arrTestKind = [...arrTestKind];
+  }
+
   getConfig();
 </script>
 
@@ -113,22 +175,32 @@
                 labelText="Name"
                 bind:value={arr.Name}
                 disabled={inputDisabled}
+                on:input={() => {
+                  UntestArr(i);
+                }}
               />
               <TextInput
                 labelText="URL"
                 bind:value={arr.URL}
                 disabled={inputDisabled}
+                on:input={() => {
+                  UntestArr(i);
+                }}
               />
               <TextInput
                 labelText="APIKey"
                 bind:value={arr.APIKey}
                 disabled={inputDisabled}
+                on:input={() => {
+                  UntestArr(i);
+                }}
               />
               <Dropdown
                 titleText="Type"
                 selectedId={arr.Type}
                 on:select={(e) => {
                   config.Arrs[i].Type = e.detail.selectedId;
+                  UntestArr(i);
                 }}
                 items={[
                   { id: "Sonarr", text: "Sonarr" },
@@ -145,6 +217,17 @@
                 icon={TrashCan}
                 iconDescription="Delete Arr"
               />
+              <Button
+                style="margin-top: 10px;"
+                on:click={() => {
+                  TestArr(i);
+                }}
+                disabled={arrTesting[i]}
+                kind={arrTestKind[i]}
+                icon={arrTestIcons[i]}
+              >
+                Test
+              </Button>
             </FormGroup>
           {/each}
         {/if}
@@ -154,6 +237,14 @@
       </Button>
     </Column>
     <Column>
+      <h4>Premiumize.me Settings</h4>
+      <FormGroup>
+        <TextInput
+          disabled={inputDisabled}
+          labelText="API Key"
+          bind:value={config.PremiumizeAPIKey}
+        />
+      </FormGroup>
       <h4>Directory Settings</h4>
       <FormGroup>
         <TextInput
@@ -210,10 +301,25 @@
   bind:open={errorModal}
   on:open={errorModal}
   passiveModal
-  modalHeading="Error Saving Config"
+  modalHeading={errorTitle}
   on:close={() => {
     errorModal = false;
   }}
 >
   <p>{errorMessage}</p>
 </Modal>
+<!-- 
+
+{() => {
+                  console.log(testStatus.get(i));
+                  if (testStatus.get(i) == undefined)
+                    return "secondary";
+                  
+                    if (testStatus.get(i) === 3) {
+                    return "danger";
+                  } else {
+                    return "secondary";
+                  }
+                }}
+
+-->

jannikw/Premiumizearr-Nova

web/src/pages/Config.svelte
@@ -13,6 +13,9 @@
     CheckmarkFilled,
     AddFilled,
     TrashCan,
+    HelpFilled,
+    MisuseOutline,
+    WatsonHealthRotate_360,
   } from "carbon-icons-svelte";
   import { CalculateAPIPath } from "../Utilities/web_root";
 
@@ -26,10 +29,17 @@
     SimultaneousDownloads: 0,
     Arrs: [],
   };
+  const ERR_SAVE = "Error Saving Config";
+  const ERR_TEST = "Error Testing *arr client";
+
+  let arrTesting = [];
+  let arrTestIcons = [];
+  let arrTestKind = [];
 
   let inputDisabled = true;
 
   let errorModal = false;
+  let errorTitle = ERR_SAVE;
   let errorMessage = "";
 
   let saveIcon = Save;
@@ -39,6 +49,12 @@
     fetch(CalculateAPIPath("api/config"))
       .then((response) => response.json())
       .then((data) => {
+        if (Array.isArray(data.Arrs)) {
+          for (let i = 0; i < data.Arrs.length; i++) {
+            SetTestArr(i, HelpFilled, "secondary", false);
+          }
+        }
+
         config = data;
         inputDisabled = false;
       })
@@ -66,14 +82,16 @@
           }, 1000);
         } else {
           errorMessage = data.status;
+          errorTitle = ERR_SAVE;
           errorModal = true;
           getConfig();
         }
       })
       .catch((error) => {
         console.error("Error: ", error);
-        errorModal = true;
+        errorTitle = ERR_SAVE;
         errorMessage = error;
+        errorModal = true;
         setTimeout(() => {
           getConfig();
         }, 1500);
@@ -97,6 +115,59 @@
     config.Arrs = [...config.Arrs];
   }
 
+  function TestArr(index) {
+    SetTestArr(index, WatsonHealthRotate_360, "secondary", true);
+
+    fetch(CalculateAPIPath("api/testArr"), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(config.Arrs[index]),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.succeeded) {
+          SetTestArr(index, CheckmarkFilled, "primary", false);
+          ResetArrTestDelayed(index, 10);
+        } else {
+          SetTestArr(index, MisuseOutline, "danger", false);
+          ResetArrTestDelayed(index, 5);
+          errorTitle = ERR_TEST;
+          errorMessage = data.status;
+          errorModal = true;
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+        SetTestArr(index, MisuseOutline, "danger", false);
+        ResetArrTestDelayed(index, 5);
+        errorTitle = ERR_TEST;
+        errorMessage = error;
+        errorModal = true;
+      });
+  }
+
+  function UntestArr(index) {
+    SetTestArr(index, HelpFilled, "secondary", false);
+  }
+
+  function SetTestArr(index, icon, kind, testing) {
+    arrTesting[index] = testing;
+    arrTestIcons[index] = icon;
+    arrTestKind[index] = kind;
+
+    arrTesting = [...arrTesting];
+    arrTestIcons = [...arrTestIcons];
+    arrTestKind = [...arrTestKind];
+  }
+
+  function ResetArrTestDelayed(index, seconds) {
+    setTimeout(() => {
+      SetTestArr(index, HelpFilled, "secondary", false);
+    }, 1000 * seconds);
+  }
+  
   getConfig();
 </script>
 
@@ -113,22 +184,32 @@
                 labelText="Name"
                 bind:value={arr.Name}
                 disabled={inputDisabled}
+                on:input={() => {
+                  UntestArr(i);
+                }}
               />
               <TextInput
                 labelText="URL"
                 bind:value={arr.URL}
                 disabled={inputDisabled}
+                on:input={() => {
+                  UntestArr(i);
+                }}
               />
               <TextInput
                 labelText="APIKey"
                 bind:value={arr.APIKey}
                 disabled={inputDisabled}
+                on:input={() => {
+                  UntestArr(i);
+                }}
               />
               <Dropdown
                 titleText="Type"
                 selectedId={arr.Type}
                 on:select={(e) => {
                   config.Arrs[i].Type = e.detail.selectedId;
+                  UntestArr(i);
                 }}
                 items={[
                   { id: "Sonarr", text: "Sonarr" },
@@ -145,6 +226,17 @@
                 icon={TrashCan}
                 iconDescription="Delete Arr"
               />
+              <Button
+                style="margin-top: 10px;"
+                on:click={() => {
+                  TestArr(i);
+                }}
+                disabled={arrTesting[i]}
+                kind={arrTestKind[i]}
+                icon={arrTestIcons[i]}
+              >
+                Test
+              </Button>
             </FormGroup>
           {/each}
         {/if}
@@ -154,6 +246,14 @@
       </Button>
     </Column>
     <Column>
+      <h4>Premiumize.me Settings</h4>
+      <FormGroup>
+        <TextInput
+          disabled={inputDisabled}
+          labelText="API Key"
+          bind:value={config.PremiumizemeAPIKey}
+        />
+      </FormGroup>
       <h4>Directory Settings</h4>
       <FormGroup>
         <TextInput
@@ -210,10 +310,25 @@
   bind:open={errorModal}
   on:open={errorModal}
   passiveModal
-  modalHeading="Error Saving Config"
+  modalHeading={errorTitle}
   on:close={() => {
     errorModal = false;
   }}
 >
   <p>{errorMessage}</p>
 </Modal>
+<!-- 
+
+{() => {
+                  console.log(testStatus.get(i));
+                  if (testStatus.get(i) == undefined)
+                    return "secondary";
+                  
+                    if (testStatus.get(i) === 3) {
+                    return "danger";
+                  } else {
+                    return "secondary";
+                  }
+                }}
+
+-->